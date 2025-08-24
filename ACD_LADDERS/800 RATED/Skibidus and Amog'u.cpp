5e0b65a1cf1c491e2391b4f21a41fb206d6b0f41
Animesh_Jantwal <animeshjantwalcoding@gmail.com>
2025-08-24 22:06:31 +0530

    800 RATED

---
+++ ACD_LADDERS/800 RATED/Skibidus and Amog'u.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string s;
+        cin>>s;
+        
+        if (s.size() >= 2 && s.substr(s.size() - 2) == "us") {
+        s.replace(s.size() - 2, 2, "i");
+        }
+        cout<<s<<endl;
+    }
+}
